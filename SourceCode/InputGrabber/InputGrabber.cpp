9b19e574b46c95bd240f19ddd540b95608fb84ba
DESKTOP-97TO8KI\James <jamesmcneil42@hotmail.co.uk>
2020-12-13 15:43:20 +0000

    Add input grabbing to put things in the right general form immediately given it's done in every class normally

--- SourceCode/InputGrabber/InputGrabber.cpp
+++ SourceCode/InputGrabber/InputGrabber.cpp
@@ -1,8 +1,9 @@
 #include "InputGrabber.h"
 #include <fstream>
 
-using std::ifstream;
 using std::getline;
+using std::ifstream;
+using std::stoi;
 
 const string InputGrabber::GetTextInput(string fileLocation)
 {
@@ -17,4 +18,60 @@ const string InputGrabber::GetTextInput(string fileLocation)
 		std::istreambuf_iterator<char>());
 
 	return str;
+}
+
+const vector<string> InputGrabber::GetEachStringLine(string fileAddress)
+{
+	string input = GetTextInput(fileAddress);
+	vector<string> entries;
+	string line = "";
+
+	for (int i = 0, len = input.length(); i < len; i++)
+	{
+		bool newLine = false;
+		if (input[i] != '\n')
+		{
+			line += input[i];
+		}
+		else
+		{
+			newLine = true;
+		}
+
+		if ((newLine || i == len - 1) && line != "")
+		{
+			entries.push_back(line);
+			line.clear();
+		}
+	}
+
+	return entries;
+}
+
+const vector<int> InputGrabber::GetEachIntLine(string fileAddress)
+{
+	string input = GetTextInput(fileAddress);
+	vector<int> entries;
+	string line = "";
+
+	for (int i = 0, len = input.length(); i < len; i++)
+	{
+		bool newLine = false;
+		if (input[i] != '\n')
+		{
+			line += input[i];
+		}
+		else
+		{
+			newLine = true;
+		}
+
+		if ((newLine || i == len - 1) && line != "")
+		{
+			entries.push_back(stoi(line));
+			line.clear();
+		}
+	}
+
+	return entries;
 }
--- SourceCode/InputGrabber/InputGrabber.h
+++ SourceCode/InputGrabber/InputGrabber.h
@@ -1,10 +1,15 @@
 #pragma once
 
 #include <string>
+#include <vector>
+
 using std::string;
+using std::vector;
 
 static class InputGrabber
 {
 public:
 	static const string GetTextInput(string fileAddress);
+	static const vector<string> GetEachStringLine(string fileAddress);
+	static const vector<int> GetEachIntLine(string fileAddress);
 };
