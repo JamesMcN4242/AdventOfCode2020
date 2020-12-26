5efd4867984b85563537bda659aa4b3569db72bc
DESKTOP-97TO8KI\James <jamesmcneil42@hotmail.co.uk>
2020-12-26 20:19:47 +0000

    Day 20 part 2.

--- SourceCode/Day20/DayTwenty.cpp
+++ SourceCode/Day20/DayTwenty.cpp
@@ -23,7 +23,9 @@ const unsigned long long DayTwentyTask::GetPartOneCode(vector<string> input)
 
 const unsigned long long DayTwentyTask::GetPartTwoCode(vector<string> input)
 {
-	return 0;
+	vector<Tile> tiles = GetTiles(input);
+	Tile map = MakeIntoOneMap(tiles);
+	return GetWaterRoughness(map);
 }
 
 const vector<Tile> DayTwentyTask::GetTiles(vector<string>& input)
@@ -85,4 +87,228 @@ const vector<Tile> DayTwentyTask::GetCornerPieces(vector<Tile>& tiles)
 	}
 
 	return corners;
+}
+
+const Tile DayTwentyTask::MakeIntoOneMap(vector<Tile>& tiles)
+{
+	ConnectPieces(tiles);
+	vector<Tile> corners = GetCornerPieces(tiles);
+	Tile tileToWorkWith;
+
+	//Get the first top left corner. Defined as a tile with (-1, x, y, -1) connections 
+	for (auto it = corners.begin(); it != corners.end(); it++)
+	{
+		if (it->connections[0] == -1 && it->connections[3] == -1)
+		{
+			tileToWorkWith = *it;
+			break;
+		}
+	}
+
+	vector<vector<Tile>> tileMap;
+	while (tileToWorkWith.ConnectionsMade() > 0)
+	{
+		vector<Tile> tileLine;
+
+		while (tileToWorkWith.ConnectionsMade() > 0)
+		{
+			tileLine.push_back(tileToWorkWith);
+			tileToWorkWith = GetNextOrDefaultTile(tileToWorkWith, tiles);
+		}
+
+		tileMap.push_back(tileLine);
+		tileToWorkWith = GetLowerOrDefaultTile(tileMap[tileMap.size() - 1][0], tiles);
+	}
+
+	const int tileLineCount = tileMap[0].size();
+	const int charactersInLine = tileLineCount * tileMap[0][0].internals.size();
+
+	string externalLines;
+	for (int i = 0; i < charactersInLine + 2; i++)
+	{
+		externalLines += "E";
+	}
+
+	string mapTile = "Tile 0:\n" + externalLines + "\n";
+	for (int i = 0, outsideLen = tileMap.size(); i < outsideLen; i++)
+	{
+		vector<Tile> tilesInLine = tileMap[i];
+		for (int j = 0, jLen = tilesInLine[0].internals.size(); j < jLen; j++)
+		{
+			mapTile += "E";
+			for (int k = 0; k < tileLineCount; k++)
+			{
+				mapTile += tilesInLine[k].internals[j];
+			}
+
+			mapTile += "E";
+
+			if (j < jLen - 1)
+			{
+				mapTile += "\n";
+			}
+		}
+
+		mapTile += "\n";
+	}
+
+	mapTile += externalLines;
+	return Tile(mapTile);
+}
+
+const Tile DayTwentyTask::GetNextOrDefaultTile(Tile startTile, vector<Tile>& nonOrientedTiles)
+{
+	int connectionNum = startTile.connections[1];
+	if (connectionNum == -1) return Tile();
+
+	for (auto it = nonOrientedTiles.begin(); it != nonOrientedTiles.end(); it++)
+	{
+		if (it->tileNumber == connectionNum)
+		{
+			OrientTile(startTile, *it);
+			return *it;
+		}
+	}
+}
+
+const Tile DayTwentyTask::GetLowerOrDefaultTile(Tile startTile, vector<Tile>& nonOrientedTiles)
+{
+	int connectionNum = startTile.connections[2];
+	if(connectionNum == -1) return Tile();
+
+	for (auto it = nonOrientedTiles.begin(); it != nonOrientedTiles.end(); it++)
+	{
+		if (it->tileNumber == connectionNum)
+		{
+			OrientTile(startTile, *it);
+			return *it;
+		}
+	}
+}
+
+const void DayTwentyTask::OrientTile(Tile& correctTile, Tile& toOrientAccordingly)
+{
+	//We only care to be connecting right or down at current
+	int correctTileId = correctTile.tileNumber;
+	int connectionPos = correctTile.connections[1] == toOrientAccordingly.tileNumber ? 1 : 2;
+	int shouldConnectTo = (connectionPos + 2) % 4;
+
+	for (int i = 0; i < 4; i++)
+	{
+		if (toOrientAccordingly.connections[shouldConnectTo] == correctTileId)
+		{
+			if (correctTile.sides[connectionPos] == toOrientAccordingly.sides[shouldConnectTo])
+			{
+				//Now oriented successfully!
+				return;
+			}
+			
+			//Going to have to flip it to get this done
+			break;
+		}
+
+		toOrientAccordingly.RotateClockwise();
+	}
+
+	toOrientAccordingly.RotateClockwise();
+	toOrientAccordingly.FlipTile();
+	OrientTile(correctTile, toOrientAccordingly);
+}
+
+const unsigned long long DayTwentyTask::GetWaterRoughness(Tile& map)
+{
+	const string seaMonster = "                  # \n#    ##    ##    ###\n #  #  #  #  #  #   ";
+	vector<string> seaMonsterVec = InputGrabber::GetEachLine(seaMonster, '\n');
+
+	bool spotted = false;
+	for (int i = 0; i < 4; i++)
+	{
+		if (SpottedSeaMonsters(map, seaMonsterVec))
+		{
+			spotted = true;
+			break;
+		}
+		map.RotateClockwise();
+	}
+
+	if (!spotted)
+	{
+		map.RotateClockwise();
+		map.FlipTile();
+		return GetWaterRoughness(map);
+	}
+
+	unsigned long long roughness = 0;
+	const int lineSize = map.internals[0].size();
+	for (int i = 0, lineCount = map.internals.size(); i < lineCount; i++)
+	{
+		for (int j = 0; j < lineSize; j++)
+		{
+			if (map.internals[i][j] == '#')
+			{
+				roughness++;
+			}
+		}
+	}
+	return roughness;
+}
+
+const bool DayTwentyTask::SpottedSeaMonsters(Tile& mapTile, vector<string>& seaMonster)
+{
+	int seaMonstersFound = 0;
+
+	vector<string>& internals = mapTile.internals;
+	const int lineCountToCheck = internals.size() - seaMonster.size();
+	const int lineSizeToCheck = internals[0].size() - seaMonster[0].size();
+
+	for (int i = 0; i <= lineCountToCheck; i++)
+	{
+		for (int j = 0; j <= lineSizeToCheck; j++)
+		{
+			if (IsSeaMonsterPresent(internals, i, j, seaMonster))
+			{
+				WriteSeaMonsterAtLocation(internals, i, j, seaMonster);
+				seaMonstersFound++;
+			}
+		}
+	}
+
+	return seaMonstersFound > 0;
+}
+
+const bool DayTwentyTask::IsSeaMonsterPresent(vector<string>& internals, int startLine, int startCharIndex, vector<string>& seaMonster)
+{
+	const int lineSize = seaMonster[0].size();
+
+	for (int i = 0, lineCount = seaMonster.size(); i < lineCount; i++)
+	{
+		for (int j = 0; j < lineSize; j++)
+		{
+			if (seaMonster[i][j] == '#')
+			{
+				if (internals[startLine + i][startCharIndex + j] != '#')
+				{
+					return false;
+				}
+			}
+		}
+	}
+
+	return true;
+}
+
+const void DayTwentyTask::WriteSeaMonsterAtLocation(vector<string>& internals, int startLine, int startCharIndex, vector<string>& seaMonster)
+{
+	const int lineSize = seaMonster[0].size();
+
+	for (int i = 0, lineCount = seaMonster.size(); i < lineCount; i++)
+	{
+		for (int j = 0; j < lineSize; j++)
+		{
+			if (seaMonster[i][j] == '#')
+			{
+				internals[startLine + i][startCharIndex + j] = 'O';
+			}
+		}
+	}
 }
--- SourceCode/Day20/DayTwenty.h
+++ SourceCode/Day20/DayTwenty.h
@@ -14,10 +14,12 @@ struct Tile
 public:
 	Tile(string input = "")
 	{
+		fill_n(connections, 4, -1);
+		if (input == "") return;
+
 		auto colonIndex = input.find(':');
 		auto spaceIndex = input.find(' ');
 		tileNumber = stoi(input.substr(spaceIndex + 1, colonIndex - spaceIndex));
-		fill_n(connections, 4, -1);
 
 		input = input.substr(colonIndex + 1, input.size() - colonIndex - 1);
 		vector<string> lines = InputGrabber::GetEachLine(input, '\n');
@@ -32,6 +34,11 @@ public:
 			{
 				sides[1] += lines[i][lineSize - 1];
 				sides[3] += lines[i][0];
+
+				if (i > 0 && i < lineCount - 1)
+				{
+					internals.push_back(lines[i].substr(1, lineSize - 2));
+				}
 			}
 		}
 	}
@@ -49,9 +56,58 @@ public:
 		return count;
 	}
 
+	void RotateClockwise()
+	{
+		int temp = connections[0];
+		connections[0] = connections[3];
+		connections[3] = connections[2];
+		connections[2] = connections[1];
+		connections[1] = temp;
+
+		string strTemp = sides[0];
+		sides[0] = sides[3];
+		sides[3] = sides[2];
+		sides[2] = sides[1];
+		sides[1] = strTemp;
+
+		reverse(sides[0].begin(), sides[0].end());
+		reverse(sides[2].begin(), sides[2].end());
+
+		vector<string> newInternals;
+		newInternals.resize(internals.size());
+		for (int i = internals.size() - 1; i >= 0; i--)
+		{
+			for (int j = 0, jLen = internals[i].size(); j < jLen; j++)
+			{
+				newInternals[j] += internals[i][j];
+			}
+		}
+
+		internals = newInternals;
+	}
+
+	void FlipTile()
+	{
+		int temp = connections[1];
+		connections[1] = connections[3];
+		connections[3] = temp;
+
+		string strTemp = sides[1];
+		sides[1] = sides[3];
+		sides[3] = strTemp;
+		reverse(sides[0].begin(), sides[0].end());
+		reverse(sides[2].begin(), sides[2].end());
+
+		for (int i = 0, len = internals.size(); i < len; i++)
+		{
+			reverse(internals[i].begin(), internals[i].end());
+		}
+	}
+
 	int tileNumber;
 	string sides[4];
 	int connections[4];
+	vector<string> internals;
 };
 
 class DayTwentyTask
@@ -63,4 +119,12 @@ private:
 	static const vector<Tile> GetTiles(vector<string>& input);
 	static const void ConnectPieces(vector<Tile>& tiles);
 	static const vector<Tile> GetCornerPieces(vector<Tile>& tiles);
+	static const Tile MakeIntoOneMap(vector<Tile>& tiles);
+	static const Tile GetLowerOrDefaultTile(Tile startTile, vector<Tile>& nonOrientedTiles);
+	static const Tile GetNextOrDefaultTile(Tile startTile, vector<Tile>& nonOrientedTiles);
+	static const void OrientTile(Tile& correctTile, Tile& toOrientAccordingly);
+	static const unsigned long long GetWaterRoughness(Tile& mapTile);
+	static const bool SpottedSeaMonsters(Tile& mapTile, vector<string>& seaMonster);
+	static const bool IsSeaMonsterPresent(vector<string>& internals, int startLine, int startCharIndex, vector<string>& seaMonster);
+	static const void WriteSeaMonsterAtLocation(vector<string>& internals, int startLine, int startCharIndex, vector<string>& seaMonster);
 };
